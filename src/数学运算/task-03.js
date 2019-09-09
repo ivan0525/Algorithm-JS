8e33cdb45dd89f3dd0015cac0cb4e0ce5e84d23c
solophone <solophone@aliyun.com>
2019-09-09 09:02:21 +0800

    add task-03.js

---
+++ src/数学运算/task-03.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+  if (n === 0) return false
+  if (n === 1) return 1
+  if (n === 2) return 2
+  return climbStairs(n - 1) + climbStairs(n - 2)
+}
+
+console.log(climbStairs(4))
